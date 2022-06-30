6ccd0de436126ded414d0031ba6265960a10f811
wprestonneal <mrprestonneal@gmail.com>
2022-06-29 18:48:30 -0700

    update new.py to replace spaces with hyphens in input

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 from sys import argv
 
 if len(argv) < 2:
-   problem_name = input('Problem name:')
+   problem_name = input('Problem name:').replace(' ', '-')
 else:
-    problem_name = argv[1]
+    problem_name = argv[1].replace(' ', '-')
  
 file_contents = '"""\nProblem:\n\nv1:\n\n"""\n\n\n"""\nResults:\n\n"""'
 with open(f'{problem_name}.py', 'w') as file:
