cb60cf9f486c6e8aa9427d77ce70966082790e39
Preston Neal <mrprestonneal@gmail.com>
2025-03-16 09:25:26 -0700

    pushing old stuff

---
+++ valid-anagram.py
@@ -0,0 +1,16 @@
+"""
+Problem:
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+    typically using all the original letters exactly once.
+v1:
+
+"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return
+
+"""
+Results:
+
+"""
